25fae25fd9ec87cb15bd350a287ad6ec7809db61
Zaid Ibrahim <zibrahim90@gmail.com>
2017-10-15 16:29:02 -0700

    first commit

---
+++ src/parser/parser.py
@@ -0,0 +1,21 @@
+
+
+class Parser:
+
+
+
+
+    def __init__(self):
+
+
+    
+    def parse(self, file_name):
+        # Open the file
+        # Read the lines in from that file as one long string
+
+        # Break that up into a list of trigrams
+
+        # return that list
+
+    
+
