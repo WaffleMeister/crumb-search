4a9126de0a988baaf6f06139d83f5d8d3e3a9b31
Zaid Ibrahim <zibrahim90@outlook.com>
2018-12-23 15:21:18 -0800

    First cut at adding in multiple word support.

--- src/trigramIndexer/parser/word_breaker.py
+++ src/trigramIndexer/parser/word_breaker.py
@@ -17,6 +17,8 @@ class WordBreaker:
 
         current_char = search_term[idx]
 
+        # Have to look at previous character since the special char
+        # can be escaped.
         if WordBreaker.__is_special_char(search_term, idx):
             """
                 For '?' characters, create two versions of 
@@ -25,17 +27,27 @@ class WordBreaker:
 
                 rob?in => roin, robin
                 batm?an => batan, batman
+
+
+                bat(man|woman) => batman, batwoman
+
+                When you see a paren, skip ahead until you find a | character
+                then, go on until you close that paren
+
+                take those two substrings, and return those....
             """
-            # Do not include the question mark; skip over it.
-            WordBreaker.break_apart_search_term(search_term,
-                                    idx + 1,
-                                    created_term,
-                                    return_list)
-            # Skip over the question mark, and also remove the last character
-            WordBreaker.break_apart_search_term(search_term,
-                                    idx + 1,
-                                    created_term[0:-1],
-                                    return_list)
+            if current_char == '?':
+                # Do something for ? characters
+                WordBreaker.__handle_question_mark(
+                    search_term, idx, created_term, return_list)
+            if current_char == '(':
+                pass
+                # This is an opening bracket, which deliniates an OR operation
+                # for a regex.
+
+                # That's going to be different for ( | ) characters,
+                # which mean some kind of split should happen.
+
         else:
             WordBreaker.break_apart_search_term(search_term,
                                                 idx + 1,
@@ -43,6 +55,31 @@ class WordBreaker:
                                                 return_list)
 
         return set(return_list)
+
+    @staticmethod
+    def __handle_opening_bracket(search_term,
+                                 idx,
+                                 created_term,
+                                 return_list):
+        pass
+
+    @staticmethod
+    def __handle_question_mark(search_term,
+                               idx,
+                               created_term,
+                               return_list):
+        # Do not include the question mark; skip over it.
+        WordBreaker.break_apart_search_term(search_term,
+                                            idx + 1,
+                                            created_term,
+                                            return_list)
+        # Skip over the question mark, and also remove the last character
+        WordBreaker.break_apart_search_term(search_term,
+                                            idx + 1,
+                                            created_term[0:-1],
+                                            return_list)
+
+        print("Do nothing")
 
     @staticmethod
     def __is_special_char(search_term, idx):
